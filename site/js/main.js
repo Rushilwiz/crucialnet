e5932bc88f548e47c54ab555cc8899652b1f972a
rushilwiz <rushilwiz@gmail.com>
2020-03-17 13:47:22 -0400

    initial commit

---
+++ site/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#titleToQuote").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#quote-section").offset().top}, 1250);
+    });
+    
+    $('.quoteToAbout, #toAbout').click(function(){
+       $('html, body').animate({
+           scrollTop: $("#about-section").offset().top}, 1250);
+    }); 
+     
+     $('.menu').click(function() {
+          $('ul').toggleClass('active');
+     })
+});
