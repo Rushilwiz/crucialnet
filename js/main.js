7d5b7735c7a12e001c7da2254d71b87a229cd635
rushilwiz <rushilwiz@gmail.com>
2020-07-04 14:31:41 -0400

    added preload for transitions

--- js/main.js
+++ js/main.js
@@ -3,13 +3,17 @@ $(document).ready(function(){
         $('html, body').animate({
             scrollTop: $("#quote-section").offset().top}, 1250);
     });
-    
+
     $('.quoteToAbout, #toAbout').click(function(){
        $('html, body').animate({
            scrollTop: $("#about-section").offset().top}, 1250);
-    }); 
-     
+    });
+
      $('.menu').click(function() {
           $('ul').toggleClass('active');
      })
+});
+
+$(window).load(function() {
+  $("body").removeClass("preload");
 });
